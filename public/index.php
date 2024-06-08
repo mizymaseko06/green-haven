b375475774e97959150c7308a77086c20daac353
Mayenziwe Maseko <86263980+mizymaseko06@users.noreply.github.com>
2024-06-08 12:30:37 +0200

    Styled hero section
    Added text content
    Added picture background
    Set height to 100vh to fill screen at start-up
    Aligned content to center to place it on white area

--- public/index.php
+++ public/index.php
@@ -4,14 +4,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Phakela</title>
+    <title>Home - Green Haven</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 
 <body>
     <header></header>
-    <main></main>
+    <main>
+        <img id="hero-bg" src="../assets/images/hero-background.png" alt="">
+        <section id="hero">
+            <div class="container">
+                <div class="row align-content-center">
+                    <div class="col-6">
+                        <h1 class="display-4 fw-bold">Welcome to <span style="color: var(--light-green)">Green Haven</span></h1>
+                        <p class="lead">Indulge in fresh, flavourful vegetarian cuisine crafted from the finest locally sourced ingredients</p>
+                        <a href="" class="btn btn-primary action-btn">View menu</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
     <footer></footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="../assets/js/script.js"></script>
