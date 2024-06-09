61f02d276501bb2b9497b4aa671b672388250b3d
Mayenziwe Maseko <86263980+mizymaseko06@users.noreply.github.com>
2024-06-09 13:51:32 +0200

    Added basic styling and content to review card

--- public/index.php
+++ public/index.php
@@ -54,6 +54,29 @@
                 </div>
             </div>
         </section>
+
+        <section id="reviews">
+            <div class="container">
+                <h4 class="text-center mt-5 text-light">REVIEWS</h4>
+                <p class="display-6 text-center">What others have to say</p>
+                <div class="row card-deck">
+                    <?php
+                    for ($i = 0; $i < 4; $i++) { ?>
+                        <article class="card col-3 m-5">
+                            <div class="card-body">
+                                <p class="card-text">
+                                    “The food is amazing, I wish I had discovered it earlier. I love that there is a wide variety of meals. Also, their site is well-run and designed!”
+                                </p>
+                            </div>
+                            <div class="card-footer bg-light">
+                                <p class="">Mayenziwe Maseko</p>
+                            </div>
+                        </article>
+                    <?php
+                    } ?>
+                </div>
+            </div>
+        </section>
     </main>
     <footer></footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
