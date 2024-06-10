e05ffcc948d0e31bc746f75205cf511ced483dc0
Mayenziwe Maseko <86263980+mizymaseko06@users.noreply.github.com>
2024-06-10 17:28:13 +0200

    Added detailed menu + some minor changes
    Added nav tabs
    Modified nav bar links

--- includes/header.php
+++ includes/header.php
@@ -15,9 +15,9 @@
 
             <div class="collapse navbar-collapse justify-content-end" id="NavigationMenu">
                 <ul class="navbar-nav me-5 pe-5">
-                    <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
+                    <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
                     <li class="nav-item"><a href="menu.php" class="nav-link">Menu</a></li>
-                    <li class="nav-item"><a href="#reviews" class="nav-link">Reviews</a></li>
+                    <li class="nav-item"><a href="index.php#reviews" class="nav-link">Reviews</a></li>
                 </ul>
                 <div>
                     <i class="bi bi-cart me-5">E2.00</i>
---
+++ public/menu.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Menu - Green Haven</title>
+    <?php
+    include("../includes/header.php");
+    ?>
+    <main>
+        <section id="menu">
+            <div class="container">
+                <h4 class="text-center" style="z-index: 1;">MENU</h4>
+                <p class="display-6 text-center">What the chef has prepared for today</p>
+
+                <div>
+                    <ul class="nav nav-tabs bg-light" id="myTab" role="tablist">
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link active" id="all-tab" data-bs-toggle="tab" data-bs-target="#all" type="button" role="tab" aria-controls="all">All</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="starters-tab" data-bs-toggle="tab" data-bs-target="#starters" type="button" role="tab" aria-controls="starters">Starters</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="main-tab" data-bs-toggle="tab" data-bs-target="#main" type="button" role="tab" aria-controls="main">Main</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="sides-tab" data-bs-toggle="tab" data-bs-target="#sides" type="button" role="tab" aria-controls="sides">Sides</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="desserts-tab" data-bs-toggle="tab" data-bs-target="#desserts" type="button" role="tab" aria-controls="desserts">Desserts</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="beverages-tab" data-bs-toggle="tab" data-bs-target="#beverages" type="button" role="tab" aria-controls="beverages">Beverages</button>
+                        </li>
+                    </ul>
+                    <div class="tab-content" id="myTabContent">
+                        <div class="tab-pane fade show active" id="all" role="tabpanel" aria-labelledby="all-tab">...</div>
+                        <div class="tab-pane fade" id="starters" role="tabpanel" aria-labelledby="starters-tab">...</div>
+                        <div class="tab-pane fade" id="main" role="tabpanel" aria-labelledby="main-tab">...</div>
+                        <div class="tab-pane fade" id="sides" role="tabpanel" aria-labelledby="sides-tab">...</div>
+                        <div class="tab-pane fade" id="desserts" role="tabpanel" aria-labelledby="desserts-tab">...</div>
+                        <div class="tab-pane fade" id="beverages" role="tabpanel" aria-labelledby="beverages-tab">...</div>
+                    </div>
+
+                </div>
+                <div class="row card-deck mt-5">
+                    <?php
+                    for ($i = 0; $i < 6; $i++) {
+                    ?>
+                        <article class="card col-8 col-md-4 col-lg-3">
+                            <div class="card-img-top">
+                                <img src="../assets/images/lefteris-kallergis-_TLKIVSW6Do-unsplash.jpg" class="img-fluid" alt="">
+                            </div>
+                            <div class="card-body text-center">
+                                <p class="card-title">Veggie Burger</p>
+                                <p class="card-text">E129.99</p>
+                                <div class="qty-container">
+                                    <span>-</span>
+                                    <span>3</span>
+                                    <span>+</span>
+                                </div>
+                                <a href="" class="btn btn-primary action-btn w-100">Add to cart</a>
+                            </div>
+                        </article>
+                    <?php }
+                    ?>
+                </div>
+            </div>
+            </div>
+        </section>
+    </main>
+    <footer></footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="../assets/js/script.js"></script>
+    </body>
+
+</html>
