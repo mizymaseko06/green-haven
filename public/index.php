b74877e050ec3ad15ecb63cbc64d1645a1ae4e20
Mayenziwe Maseko <86263980+mizymaseko06@users.noreply.github.com>
2024-06-10 11:46:33 +0200

    Added nav bar + some other small changes to menu preview section

--- public/index.php
+++ public/index.php
@@ -6,11 +6,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home - Green Haven</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 
 <body>
-    <header></header>
+    <header>
+        <nav class="navbar navbar-expand-md container">
+            <a href="" class="navbar-brand"><img src="../assets/images/green-haven-logo.png" alt=""></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#NavigationMenu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse justify-content-end" id="NavigationMenu">
+                <ul class="navbar-nav me-5 pe-5">
+                    <li class="nav-item"><a href="" class="nav-link">Home</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Menu</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Reviews</a></li>
+                </ul>
+                <div>
+                    <i class="bi bi-cart me-5">E2.00</i>
+                    <i class="bi bi-box-arrow-right"></i>
+                </div>
+
+            </div>
+
+        </nav>
+    </header>
     <main>
         <img id="hero-bg" src="../assets/images/hero-background.png" alt="">
         <section id="hero">
@@ -60,8 +82,8 @@
                 <p class="display-6 text-center">What others have to say</p>
                 <div class="row card-deck">
                     <?php
-                    for ($i = 0; $i < 4; $i++) { ?>
-                        <article class="card col-3 m-5">
+                    for ($i = 0; $i < 3; $i++) { ?>
+                        <article class="card col-10 col-md-6 col-lg-3 m-5">
                             <div class="card-body">
                                 <p class="card-text">
                                     “The food is amazing, I wish I had discovered it earlier. I love that there is a wide variety of meals. Also, their site is well-run and designed!”
