999278404af64b02b323bee37c8d3a199f4f1c5a
Mayenziwe Maseko <86263980+mizymaseko06@users.noreply.github.com>
2024-06-10 14:16:54 +0200

    Created separate file for header

---
+++ includes/header.php
@@ -0,0 +1,30 @@
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-md container">
+            <a href="" class="navbar-brand"><img src="../assets/images/green-haven-logo.png" alt=""></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#NavigationMenu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse justify-content-end" id="NavigationMenu">
+                <ul class="navbar-nav me-5 pe-5">
+                    <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
+                    <li class="nav-item"><a href="menu.php" class="nav-link">Menu</a></li>
+                    <li class="nav-item"><a href="#reviews" class="nav-link">Reviews</a></li>
+                </ul>
+                <div>
+                    <i class="bi bi-cart me-5">E2.00</i>
+                    <i class="bi bi-box-arrow-right"></i>
+                </div>
+
+            </div>
+
+        </nav>
+    </header>
--- public/index.php
+++ public/index.php
@@ -2,37 +2,10 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home - Green Haven</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="../assets/css/style.css">
-</head>
-
-<body>
-    <header>
-        <nav class="navbar navbar-expand-md container">
-            <a href="" class="navbar-brand"><img src="../assets/images/green-haven-logo.png" alt=""></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#NavigationMenu">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse justify-content-end" id="NavigationMenu">
-                <ul class="navbar-nav me-5 pe-5">
-                    <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
-                    <li class="nav-item"><a href="menu.php" class="nav-link">Menu</a></li>
-                    <li class="nav-item"><a href="#reviews" class="nav-link">Reviews</a></li>
-                </ul>
-                <div>
-                    <i class="bi bi-cart me-5">E2.00</i>
-                    <i class="bi bi-box-arrow-right"></i>
-                </div>
-
-            </div>
-
-        </nav>
-    </header>
+    <?php
+    include("../includes/header.php");
+    ?>
     <main>
         <img id="hero-bg" src="../assets/images/hero-background.png" alt="">
         <section id="hero">
@@ -102,6 +75,6 @@
     <footer></footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="../assets/js/script.js"></script>
-</body>
+    </body>
 
 </html>
